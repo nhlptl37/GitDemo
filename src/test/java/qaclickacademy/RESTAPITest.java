d3e4a1b7bdf22a42395ceab4061d31dc43bb2032
nehal <practiceselenium37@gmail.com>
2022-08-25 19:06:49 -0400

    this is my first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
