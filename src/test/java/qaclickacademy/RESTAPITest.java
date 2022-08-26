5f32cc9cb56f4227e3c3d58b737f1862e86c6b41
nehal <practiceselenium37@gmail.com>
2022-08-25 20:19:09 -0400

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -11,6 +11,13 @@ public class RESTAPITest {
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		//reg/
+		//regreg
+		//regfregreg
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
 	}
 
 	@Test
